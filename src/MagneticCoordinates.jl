3a6d39fa39f975e52ad0f197216ad74442599362
Benjamin Faber <bfaber@wisc.edu>
2020-10-29 10:57:08 -0500

    Updated the Boozer coordinate definitions

--- src/MagneticCoordinates.jl
+++ src/MagneticCoordinates.jl
@@ -156,84 +156,77 @@ Base.show(io::IO, x::PestCoordinates) = print(io, "PestCoordinates(s=$(x.ψ), α
 Base.isapprox(x1::PestCoordinates, x2::PestCoordinates; kwargs...) = isapprox(x1.ψ,x2.ψ;kwargs...) && isapprox(x1.α,x2.α;kwargs...) && isapprox(x1.ζ,x2.ζ;kwargs...)
 
 """
-    BoozerCoordinates{T,A}(ψ::T,θ::A,ζ::A)
+    BoozerCoordinates{T,A}(ψ::T,χ::A,ϕ::A)
 
 Coordinates on a magnetic flux surface, where `ψ` is the flux label and
-`θ` and `ζ` are angle-like variables
+`χ` and `ϕ` are angle-like variables
 """
 struct BoozerCoordinates{T <: Real,A <: Real} <: MagneticCoordinates
   ψ::T
   χ::A
-  ζ::A
-  BoozerCoordinates{T,A}(ψ::T,χ::A,ζ::A) where {T,A} = new(ψ,χ,ζ)
+  ϕ::A
+  BoozerCoordinates{T,A}(ψ::T,χ::A,ϕ::A) where {T,A} = new(ψ,χ,ϕ)
 end
 
-function BoozerCoordinates(ψ,χ,ζ)
-  ψ2, χ2, ζ2 = promote(ψ,χ,ζ)
-  return BoozerCoordinates{typeof(ψ2),typeof(χ2)}(ψ2,χ2,ζ2)
+function BoozerCoordinates(ψ,χ,ϕ)
+  ψ2, χ2, ϕ2 = promote(ψ,χ,ϕ)
+  return BoozerCoordinates{typeof(ψ2),typeof(χ2)}(ψ2,χ2,ϕ2)
 end
 
-function BoozerCoordinates(ψ,χ,ζ::T) where T <: AbstractVector
-  coords = Vector{BoozerCoordinates}(undef,length(ζ))
-  map!(z->BoozerCoordinates(ψ,χ,z),coords,ζ)
+function BoozerCoordinates(ψ,χ,ϕ::T) where T <: AbstractVector
+  coords = Vector{BoozerCoordinates}(undef,length(ϕ))
+  map!(z->BoozerCoordinates(ψ,χ,z),coords,ϕ)
   return coords
 end
 
-function BoozerCoordinates(ψ,χ::AbstractVector,ζ::AbstractVector)
-  coords = Matrix{BoozerCoordinates}(undef,length(ζ),length(χ))
+function BoozerCoordinates(ψ,χ::AbstractVector,ϕ::AbstractVector)
+  coords = Matrix{BoozerCoordinates}(undef,length(ϕ),length(χ))
   for x = 1:length(χ)
-    for z = 1:length(ζ)
-      @inbounds coords[z,x] = BoozerCoordinates(ψ,χ[x],ζ[z])
+    for z = 1:length(ϕ)
+      @inbounds coords[z,x] = BoozerCoordinates(ψ,χ[x],ϕ[z])
     end
   end
   return coords
 end
 
-function BoozerCoordinates(ψ,χ::AbstractMatrix,ζ::AbstractMatrix)
-  @assert size(χ) == size(ζ) "Incompatible sizes"
+function BoozerCoordinates(ψ,χ::AbstractMatrix,ϕ::AbstractMatrix)
+  @assert size(χ) == size(ϕ) "Incompatible sizes"
   coords = Matrix{BoozerCoordinates}(undef,size(χ))
-  map!((x,z)->BoozerCoordinates(ψ,x,z),coords,χ,ζ)
+  map!((x,z)->BoozerCoordinates(ψ,x,z),coords,χ,ϕ)
   return coords
 end
 
-function BoozerCoordinates(ψ,χ::AbstractMatrix,ζ::AbstractMatrix)
-  @assert size(χ) == size(ζ) "Incompatible sizes"
-  coords = Matrix{BoozerCoordinates}(undef,size(χ))
-  map!((x,z)->BoozerCoordinates(ψ,x,z),coords,χ,ζ)
-  return coords
-end
-
-function BoozerCoordinates(ψ::AbstractVector,χ::AbstractVector,ζ::AbstractVector)
-  coords = Array{BoozerCoordinates,3}(undef,length(ζ),length(χ),length(ψ))
+function BoozerCoordinates(ψ::AbstractVector,χ::AbstractVector,ϕ::AbstractVector)
+  coords = Array{BoozerCoordinates,3}(undef,length(ϕ),length(χ),length(ψ))
   for s = 1:length(ψ)
     for x = 1:length(χ)
-      for z = 1:length(ζ)
-        @inbounds coords[z,x,s] = BoozerCoordinates(ψ[s],χ[x],ζ[z])
+      for z = 1:length(ϕ)
+        @inbounds coords[z,x,s] = BoozerCoordinates(ψ[s],χ[x],ϕ[z])
       end
     end
   end
   return coords
 end
 
-function BoozerCoordinates(ψ::AbstractVector,χ::AbstractMatrix,ζ::AbstractMatrix)
-  @assert size(χ) == size(ζ) "Incompatible sizes"
+function BoozerCoordinates(ψ::AbstractVector,χ::AbstractMatrix,ϕ::AbstractMatrix)
+  @assert size(χ) == size(ϕ) "Incompatible sizes"
   coords = Array{BoozerCoordinates,3}(undef,(size(χ)...,length(ψ)))
   for s = 1:length(ψ)
     for j = 1:size(χ,2)
       for i = 1:size(χ,1)
-        coords[i,j,s] = BoozerCoordinates(ψ[s],χ[i,j],ζ[i,j])
+        coords[i,j,s] = BoozerCoordinates(ψ[s],χ[i,j],ϕ[i,j])
       end
     end
   end
   return coords
 end
 
-function BoozerCoordinates(ψ::AbstractArray{T,3},χ::AbstractArray{T,3},ζ::AbstractArray{T,3}) where T
-  @assert size(ψ) == size(χ) == size(ζ) "Incompatible sizes"
+function BoozerCoordinates(ψ::AbstractArray{T,3},χ::AbstractArray{T,3},ϕ::AbstractArray{T,3}) where T
+  @assert size(ψ) == size(χ) == size(ϕ) "Incompatible sizes"
   coords = Array{BoozerCoordinates,3}(undef,size(ψ))
-  map!((s,x,z)->BoozerCoordinates(s,x,z),coords,ψ,χ,ζ)
+  map!((s,x,z)->BoozerCoordinates(s,x,z),coords,ψ,χ,ϕ)
   return coords
 end
 
-Base.show(io::IO, x::BoozerCoordinates) = print(io, "BoozerCoordinates(s=$(x.ψ), χ=$(x.χ), ζ=$(x.ζ))")
-Base.isapprox(x1::BoozerCoordinates, x2::BoozerCoordinates; kwargs...) = isapprox(x1.ψ,x2.ψ;kwargs...) && isapprox(x1.χ,x2.χ;kwargs...) && isapprox(x1.ζ,x2.ζ;kwargs...)
+Base.show(io::IO, x::BoozerCoordinates) = print(io, "BoozerCoordinates(s=$(x.ψ), χ=$(x.χ), ϕ=$(x.ϕ))")
+Base.isapprox(x1::BoozerCoordinates, x2::BoozerCoordinates; kwargs...) = isapprox(x1.ψ,x2.ψ;kwargs...) && isapprox(x1.χ,x2.χ;kwargs...) && isapprox(x1.ϕ,x2.ϕ;kwargs...)
