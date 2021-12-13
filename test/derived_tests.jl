19754bf3baf7785cd47b3dfa09a88a2075445ffe
Benjamin Faber <bfaber@wisc.edu>
2021-12-13 12:10:06 -0600

    Fixed tests failing due to floating point error

--- test/derived_tests.jl
+++ test/derived_tests.jl
@@ -4,23 +4,33 @@
     B_vec = BasisVectors{Float64}(0.8 .+ rand(rng, 9))
     ι = 1.0 + rand(rng)
     @testset "B_norm" begin
-        @test B_norm(Contravariant(), B_vec) == 0.9589876106776953
+        @test isapprox(B_norm(Contravariant(), B_vec), 0.9589876106776953)
         @test_throws MethodError B_norm(Covariant(), B_vec)
-        @test B_norm(Covariant(), B_vec, ι) == 7.47127616783484
+        @test isapprox(B_norm(Covariant(), B_vec, ι), 7.47127616783484)
         @test B_norm(B_vec) == B_norm(Contravariant(), B_vec)
     end
 
     @testset "B_field" begin
-        @test B_field(Contravariant(), B_vec) == SVector{3,Float64}(
-            -0.7330182803485883,
-            0.6179562983133189,
-            0.021712933542331836,
+        @test prod(
+            isapprox.(
+                B_field(Contravariant(), B_vec),
+                SVector{3,Float64}(
+                    -0.7330182803485883,
+                    0.6179562983133189,
+                    0.021712933542331836,
+                ),
+            ),
         )
         @test_throws MethodError B_field(Covariant(), B_vec)
-        @test B_field(Covariant(), B_vec, ι) == SVector{3,Float64}(
-            -4.641973086777105,
-            -3.7295049271502645,
-            -4.512521073197973,
+        @test prod(
+            isapprox(
+                B_field(Covariant(), B_vec, ι),
+                SVector{3,Float64}(
+                    -4.641973086777105,
+                    -3.7295049271502645,
+                    -4.512521073197973,
+                ),
+            ),
         )
         @test B_field(B_vec) == B_field(Contravariant(), B_vec)
     end
