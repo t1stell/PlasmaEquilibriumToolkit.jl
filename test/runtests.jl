091ec4ef2c99a3bd5401aa1e7c59b55629b62474
aaroncbader <abader@engr.wisc.edu>
2022-06-01 15:29:09 -0500

    add tests of bfield

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,9 +4,11 @@ using Statistics
 using StaticArrays
 using StructArrays
 using Random123
+using CoordinateTransformations
 using Test
 
 include("magnetic_coordinate_tests.jl")
 include("transformation_tests.jl")
 include("derived_tests.jl")
+include("bfield_tests.jl")
 include("surface_tests.jl")
